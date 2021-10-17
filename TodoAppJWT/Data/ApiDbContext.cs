aa4416f556d2a02d37cf0b6e7d32335ed13f160e
Mich <michsteam1234@gmail.com>
2021-10-17 22:10:29 +0700

    Sesi 10 Hacktiv8 With JWT

---
+++ TodoAppJWT/Data/ApiDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using TodoAppJWT.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using TodoAppJWT.Data;
+using TodoAppJWT.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoAppJWT.Data
+{
+    public class ApiDbContext : IdentityDbContext
+    {
+        public virtual DbSet<ItemData> Items {get; set;}
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
